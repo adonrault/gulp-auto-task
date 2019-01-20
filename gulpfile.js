4567ef8c9de7414ce74e8a8e8c91d50e7c51e471
adonrault <antoine.donrault@gmail.com>
2019-01-20 15:31:06 +0100

    Initialisation du serveur BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -11,3 +11,13 @@ var destination = './dist'; //dossier à livrer
 // Variables BrowserSync
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
+
+// Initialisation serveur BrowserSync
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    browserSync.watch('./**/*.*').on('change', browserSync.reload);
+});
